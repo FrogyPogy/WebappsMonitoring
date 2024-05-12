81f4e564350f05398e9d686e595c0b67adf5011b
FrogyPogy <yowmanparis1@gmail.com>
2024-05-12 13:49:33 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+const express = require('express');
+const expresLayouts = require('express-ejs-layouts');
+
+const app = express();
+const port = 5000 || process.env.port;
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+// Static Files
+app.use(express.static('public'));
+
+// Templating Engine
+app.use(expresLayouts);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+    res.render('index');
+    
+});
+
+app.listen(port, () => {
+    console.log(`App Listening on port ${port}`);
+});
